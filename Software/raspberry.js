b3a55bc2a7790dc772b37eb8f7b68ca5a06f1b55
Ralf Lici <slonebrownstone@gmail.com>
2021-11-30 16:17:44 +0100

    longer trigger pulse

--- Software/raspberry.js
+++ Software/raspberry.js
@@ -16,19 +16,19 @@ rpio.pwmSetClockDivider(clockdiv);
 rpio.pwmSetRange(PIN_PWM_X, range);
 rpio.pwmSetRange(PIN_PWM_Y, range);
 
-rpio.open(PIN_REC, rpio.OUTPUT, rpio.LOW);
-rpio.open(PIN_PB, rpio.OUTPUT, rpio.LOW);
+rpio.open(PIN_REC,  rpio.OUTPUT, rpio.LOW);
+rpio.open(PIN_PB,   rpio.OUTPUT, rpio.LOW);
 rpio.open(PIN_GATE, rpio.OUTPUT, rpio.HIGH);
-rpio.open(PIN_TRIG, rpio.OUTPUT, rpio.HIGH);
+rpio.open(PIN_TRIG, rpio.OUTPUT, rpio.LOW);
 
 exports.getRange = () => {
     return range;
 }
 
 exports.trigger = () => {
-    rpio.write(PIN_TRIG, rpio.LOW);
-    rpio.usleep(500);
     rpio.write(PIN_TRIG, rpio.HIGH);
+    rpio.usleep(10000);
+    rpio.write(PIN_TRIG, rpio.LOW);
     return;
 }
 
